b2ba17ae406354dd9bbe8634e82d7c29ac787b6c
Quvonchbek <jalilovquvonchbek4@gmail.com>
2022-07-01 19:03:32 +0300

    bu yerda yangi dastur qoshildi

---
+++ sonlar_darsi.py
@@ -0,0 +1,35 @@
+
+
+# x = 3
+# y = 3.3
+# z = 5j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# x, y, z = 10, -7.25, -30
+
+# print(x , y, z)
+
+# a = 20
+# b = 40
+
+# s = a//b
+# print(s)
+
+# telefon_nomer = 94_588_91_14
+# print(f"mening telefon nomerim: {telefon_nomer}")
+
+# ism = "ALISHER"
+# yosh = 18
+# xabar = ism + ' ' +  str(yosh)  + ' yoshda'
+# print(xabar)
+
+# t_yil = int(input(" tug'ulgan yilingizni kiriting: "))
+# yosh= 2022 - t_yil
+# print("siz " + str(yosh) + " yoshdasiz")
+
+
+
